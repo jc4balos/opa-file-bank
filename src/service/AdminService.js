05d6120d7250b96b09531b58ed7990807026841f
Jc Abalos <jc4balos@gmail.com>
2024-05-13 14:02:21 +0800

    added restore folder endpoint

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -39,4 +39,24 @@ export class AdminService {
       return error;
     }
   }
+
+  async restoreFolder(folderId) {
+    try {
+      const url = new URL(backendUrl + "/api/v1/folder/restore-folder");
+      url.search = new URLSearchParams({
+        folderId: folderId,
+      });
+
+      const response = await fetch(url, {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
 }
