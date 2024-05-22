e4e8e3ec69f316090d7b7d9aaf9933f1cb8f7d54
Jc Abalos <jc4balos@gmail.com>
2024-05-22 12:20:32 +0800

    deleted unecessary files

--- src/run.js
+++
@@ -1,7 +0,0 @@
-import { Service } from "node-windows";
-
-const service = new Service({
-  name: "opaFileBankFrontendService",
-  description: "Website service of OPA File Bank",
-  script: "",
-});
